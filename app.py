6045a3f562dd1fc0608e1bea6894ee4973ea915f
jeho74 <snetdossantos513@gmail.com>
2026-04-24 11:15:34 +0200

    Fix metrics Prometheus content-type

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, jsonify
-from prometheus_client import Counter, Gauge, generate_latest
+from prometheus_client import Counter, Gauge, generate_latest, CONTENT_TYPE_LATEST
 
 app = Flask(__name__)
 
@@ -33,7 +33,7 @@ def delete_student(index):
 # 📊 metrics
 @app.route('/metrics')
 def metrics():
-    return generate_latest()
+    return generate_latest(), 200, {'Content-Type': CONTENT_TYPE_LATEST}
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
